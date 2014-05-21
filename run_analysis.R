0c15caed1bba2878563867b6c1e59f6ed05e2cd1
Elias Dorneles <eliasdorneles@gmail.com>
2014-05-21 00:53:45 -0300

    cleaning up and getting full datasets

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,31 @@
 uci.dataset.dir <- "UCI HAR Dataset/"
 
+activity.labels <- read.table(file.path(uci.dataset.dir, "activity_labels.txt"),
+                              col.names=c("levels", "labels"))
+
 features <- read.table(file.path(uci.dataset.dir, "features.txt"),
                            col.names=c("index", "features"))
-
 mean.std.features <- features[grepl("(mean|std)", features[, 2]), 1]
 
-Xtrain <- read.table(file.path(uci.dataset.dir, "train", "X_train.txt"),
-                     col.names=features$features)
-Xtrain <- Xtrain[, mean.std.features]
+load.dataset <- function (name, col.index) {
+    getFilename <- function(prefix) {
+        file.path(uci.dataset.dir, name, paste(prefix, "_", name, ".txt", sep=""))
+    }
 
-Xtest <- read.table(file.path(uci.dataset.dir, "test", "X_test.txt"),
-                     col.names=features$features)
-Xtest <- Xtest[, mean.std.features]
+    dataset <- read.table(getFilename("X"), col.names=features$features)
+    dataset <- dataset[, col.index]
 
-tidy.dataset <- rbind(Xtrain, Xtest)
+    subjects <- read.table(getFilename("subject"), col.names=c("subjects"))
+    dataset <- cbind(dataset, subjects)
+
+    y <- read.table(getFilename("y"), col.names=c("y"))
+    y$y <- factor(y$y, activity.labels$levels, activity.labels$labels)
+    dataset <- cbind(dataset, y)
+
+    dataset
+}
+
+train.set <- load.dataset("train", mean.std.features)
+test.set  <- load.dataset("test", mean.std.features)
+
+tidy.dataset <- rbind(train.set, test.set)
